564f99e7fd83002a572242666958b0f4ead85998
Jurica Preksavec <jurica.preksavec@omega-software.hr>
2023-02-06 13:08:44 +0100

    Day7: Action

--- src/Bookstore.Service/RandomBookGenerator.cs
+++ src/Bookstore.Service/RandomBookGenerator.cs
@@ -1,7 +1,24 @@
 using Rhetos.Dom.DefaultConcepts;
 using System;
 using System.Linq;
-
+/*
+using Autofac;
+using Bookstore.Service.Controllers.Rhetos520;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Rhetos;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+*/
 namespace Bookstore.Service
 {
     public class RandomBookGenerator
@@ -13,4 +30,138 @@ namespace Bookstore.Service
             repository.Bookstore.Book.Insert(books);
         }
     }
+/*
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.CustomSchemaIds(type => type.ToString()); // Allows multiple entities with the same name in different modules.
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestApp", Version = "v1" });
+                // Adding Rhetos REST API to Swagger with document name "rhetos".
+                c.SwaggerDoc("rhetos", new OpenApiInfo { Title = "Rhetos REST API", Version = "v1" });
+            });
+
+            // Using NewtonsoftJson for backward-compatibility with older versions of Rhetos.RestGenerator:
+            // legacy Microsoft DateTime serialization and
+            // byte[] serialization as JSON array of integers instead of Base64 string.
+            services.AddControllers()
+                .AddNewtonsoftJson(o =>
+                {
+                    o.UseMemberCasing();
+                    o.SerializerSettings.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.MicrosoftDateFormat;
+                    o.SerializerSettings.Converters.Add(new Rhetos.Host.AspNet.RestApi.Utilities.ByteArrayConverter());
+                });
+
+            // Adding Rhetos to AspNetCore application.
+            services.AddRhetosHost(ConfigureRhetosHostBuilder)
+                .AddAspNetCoreIdentityUser()
+                .AddHostLogging()
+                .AddHostLocalization()
+                .AddImpersonation()
+                .AddDashboard()
+                .AddRestApi(o =>
+                {
+                    o.BaseRoute = "rest";
+                    o.GroupNameMapper = (conceptInfo, controller, oldName) => "rhetos"; // OpenAPI document name.
+                });
+
+            // Configuring Authentication.
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(o => o.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return Task.CompletedTask;
+                });
+
+            services.AddLocalization()
+                .AddPortableObjectLocalization(options => options.ResourcesPath = "Localization")
+                //OrchadCore.Localization.Core uses the IMemoryCache but no default registration is provided in the
+                //AddPortableObjectLocalization method so we must register an implementation for the IMemoryCache.
+                .AddMemoryCache();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/rhetos/swagger.json", "Rhetos REST API");
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestApp v1");
+                });
+            }
+
+            app.UseRhetosRestApi();
+
+            app.UseRouting();
+
+            app.UseRequestLocalization(options =>
+            {
+                var supportedCultures = new List<CultureInfo>
+                {
+                    new CultureInfo("en"),
+                    new CultureInfo("hr")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture("en");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders = new List<IRequestCultureProvider>
+                {
+                    //The culture will be resolved based on the query parameter.
+                    //For example if we want the validation message to be translated to Croatian
+                    //we can call the POST method rest/Bookstore/Book?culture=hr and insert a json object without the 'Title' property.
+                    //It can be configured so that the culture gets resolved based on cookies or headers.
+                    new QueryStringRequestCultureProvider()
+                };
+            });
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseRhetosImpersonation();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                if (env.IsDevelopment())
+                    endpoints.MapRhetosDashboard();
+            });
+        }
+
+        private void ConfigureRhetosHostBuilder(IServiceProvider serviceProvider, IRhetosHostBuilder rhetosHostBuilder)
+        {
+            rhetosHostBuilder
+                .ConfigureRhetosAppDefaults()
+                .UseBuilderLogProviderFromHost(serviceProvider)
+                .ConfigureConfiguration(builder => builder
+                    .MapNetCoreConfiguration(Configuration)
+                    // The "local" file is intended for developer/machine-specific database connection string, and other test settings.
+                    // It should not be committed to source control (see .gitignore).
+                    .AddJsonFile("rhetos-app.local.settings.json"))
+                .ConfigureContainer(builder =>
+                {
+                    // Registering custom components for Bookstore application:
+                    builder.RegisterType<Bookstore.SmtpMailSender>().As<Bookstore.IMailSender>(); // Application uses SMTP implementation for sending mails. The registration will be overridden in unit tests by fake component.
+                    builder.Register(context => context.Resolve<Rhetos.Utilities.IConfiguration>().GetOptions<Bookstore.MailOptions>()).SingleInstance(); // Standard pattern for registering an options class.
+                    builder.RegisterType<ServerCommandsUtility520>();
+                });
+        }
+    }
+*/
 }
